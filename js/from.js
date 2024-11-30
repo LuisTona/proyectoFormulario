4f189c2fc01837f01753e6ddbcb2bdc17f2957e1
Adrian Gavela Rodriguez <adriangavela09@gmail.com>
2024-11-30 20:56:49 +0100

    subida al localstorage

--- js/from.js
+++ js/from.js
@@ -1,28 +1,50 @@
-const nombre = document.getElementById('nombre').value;
-const apellido1 = document.getElementById('apellido1').value;
-const apellido2 = document.getElementById('apellido2').value;
-const correo = document.getElementById('mail').value;
-const contraseña = document.getElementById('pass').value;
-const confirPass= document.getElementById('confirmPass').value;
-const privacidad = document.getElementById('privacidad').value;
-const errorMensaje = document.getElementById('errorNombre').value;
+const nombre = document.getElementById('nombre');
+const apellido1 = document.getElementById('apellido1');
+const apellido2 = document.getElementById('apellido2');
+const correo = document.getElementById('mail');
+const contraseña = document.getElementById('pass');
+const confirPass= document.getElementById('confirmPass');
+const privacidad = document.getElementById('privacidad');
+const errorMensaje = document.querySelectorAll('.error-message');
+
 
 document.querySelector('#formulario').addEventListener('submit', function(event) {
-    
-    if (nombre.trim() === '') {
-        errorMensaje.style.display = 'block';
-        event.preventDefault();
+    event.preventDefault();
+  
+    let campo = [
+        {campo: nombre, valor: errorMensaje[0]},
+        {campo: apellido1, valor: errorMensaje[1]},
+        {campo: correo, valor: errorMensaje[2]},
+        {campo: contraseña, valor: errorMensaje[3]},
+        {campo: confirPass, valor: errorMensaje[4]},
+        {campo: privacidad, valor: errorMensaje[5]},
+    ];
+
+
+    campo.forEach(({campo, valor}) => {
+        if(campo.value.trim() === ''){
+            valor.style.display = 'block';
+        }
+    });
+
+    let usuario = {
+        nombre: nombre.value.trim(),
+        apellido1: apellido1.value.trim(),
+        apellido2: apellido2.value.trim(),
+        correo: correo.value.trim(),
+        privacidad: privacidad.value.trim(),
+    };
+
+    if(contraseña.value === confirPass.value){
+        usuario.contraseña = contraseña.value.trim();
+    }else{
+        alert("Las contraseñas no son iguales");
     }
 
-    // if(apellido1.trim() === ''){
-    //     alert('El campo primer apellido  es obligatorio.');
-    // }
+    if(nombre.value.trim() !== '' && apellido1.value.trim() !== '' && correo.value.trim() !== '' && contraseña.value.trim() !== '' && privacidad.value.trim() !== ''){
+        localStorage.setItem('usuario', JSON.stringify(usuario));
+    }else{
+        alert("Falta algun campo por rellenar");
+    }
 
-    // if(correo.trim() === ''){
-    //     alert('El campo correo electronico es obligatorio');
-    // }
-
-    // if(contraseña.trim() === ''){
-    //     alert('El campo Contraseña es obligatorio.');
-    // }
 });
